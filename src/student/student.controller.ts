d8cd36627c54b1d8b60c9eea15b8b8c6c7b7861e
nicolesinadjan17 <nicolemarie.sinadjan@cvsu.edu.ph>
2025-01-08 11:23:30 +0800

    Update student.controller.ts

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,24 +1,30 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Put,
-    Delete,
-    Body,
-    Param,
-  } from '@nestjs/common';
-  import { StudentService } from './student.service';
-  import { Student } from './student.entity';
-  
-  @Controller('students')
-  export class StudentController {
-    constructor(private readonly studentService: StudentService) {}
-  
-    @Post()
-    async create(@Body() student: Partial<Student>): Promise<Student> {
-      return await this.studentService.create(student);
-    }
-  
- 
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+  async create(student: Partial<Student>): Promise<Student> {
+    const newStudent = this.studentRepository.create(student);
+    return await this.studentRepository.save(newStudent);
   }
-  
+
+  async findAll(): Promise<Student[]> {
+    return await this.studentRepository.find();
+  }
+
+  async update(id: number, student: Partial<Student>): Promise<Student> {
+    await this.studentRepository.update(id, student);
+    return await this.studentRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.studentRepository.delete(id);
+  }
+}
