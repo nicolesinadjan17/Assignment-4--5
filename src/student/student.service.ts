be9b8988fd46c041a23c1527a3041089c266ec5d
nicolesinadjan17 <nicolemarie.sinadjan@cvsu.edu.ph>
2025-01-08 11:25:30 +0800

    Update student.service.ts

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -14,17 +14,4 @@ export class StudentService {
     const newStudent = this.studentRepository.create(student);
     return await this.studentRepository.save(newStudent);
   }
-
-  async findAll(): Promise<Student[]> {
-    return await this.studentRepository.find();
-  }
-
-  async update(id: number, student: Partial<Student>): Promise<Student> {
-    await this.studentRepository.update(id, student);
-    return await this.studentRepository.findOne({ where: { id } });
-  }
-
-  async delete(id: number): Promise<void> {
-    await this.studentRepository.delete(id);
-  }
 }
